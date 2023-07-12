c092789bebe48c8954712a4cc6970683023d9a82
jiangyonggang@sinosoft.com.cn <532095311@qq.com>
2023-07-12 23:58:44 +0800

    【3】 解决跨域

--- build/vue-loader.conf.js
+++ build/vue-loader.conf.js
@@ -2,4 +2,15 @@
 
 module.exports = {
   //You can set the vue-loader configuration by yourself.
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:9110',
+        changeOrigin: true,
+        pathRewrite: {
+          '^/api': '/'
+        }
+      }
+    }
+  }
 }
--- config/dev.env.js
+++ config/dev.env.js
@@ -4,5 +4,17 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  BASE_API: '"https://easy-mock.com/mock/5950a2419adc231f356a6636/vue-admin"',
+  // BASE_API: '"https://easy-mock.com/mock/5950a2419adc231f356a6636/vue-admin"',
+  BASE_API: 'http://localhost:9110',
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:9110',
+        changeOrigin: true,
+        pathRewrite: {
+          '^/api': '/'
+        }
+      }
+    }
+  }
 })
